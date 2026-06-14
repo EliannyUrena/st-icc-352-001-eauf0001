fae0ce5b600a8d3d8f1201913781005ed0fbc23a
Elianny Urena <eliannyurenafdez17@gmail.com>
2026-06-14 15:27:57 -0400

    configurando javalin

--- practica-2/practica2/src/main/java/edu/pucmm/eict/p2/Main.java
+++ practica-2/practica2/src/main/java/edu/pucmm/eict/p2/Main.java
@@ -1,7 +1,25 @@
 package edu.pucmm.eict.p2;
 
+import io.javalin.Javalin;
+
 public class Main {
     void main() {
         IO.println("Hola");
+
+        Javalin app = Javalin.create(config -> {
+
+            config.staticFiles.add(staticFileConfig -> {
+                staticFileConfig.hostedPath = "/";
+                staticFileConfig.directory = "/publico";
+            });
+
+            config.routes.get("/", ctx -> {
+                ctx.result("Hola");
+            });
+        });
+
+        app.start(7000);
+
     }
 }
+
