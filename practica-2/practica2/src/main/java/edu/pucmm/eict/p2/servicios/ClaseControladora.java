adec291529c25d8fa7b3e37a47348c03b461b604
Elianny Urena <eliannyurenafdez17@gmail.com>
2026-06-14 19:20:07 -0400

    Creando metodos para crear, buscar y eliminar productos en la clase controladora

--- practica-2/practica2/src/main/java/edu/pucmm/eict/p2/servicios/ClaseControladora.java
+++ practica-2/practica2/src/main/java/edu/pucmm/eict/p2/servicios/ClaseControladora.java
@@ -1,5 +1,6 @@
 package edu.pucmm.eict.p2.servicios;
 
+import edu.pucmm.eict.p2.entidades.CarroCompra;
 import edu.pucmm.eict.p2.entidades.Producto;
 import edu.pucmm.eict.p2.entidades.Usuario;
 import edu.pucmm.eict.p2.entidades.VentaProductos;
@@ -14,6 +15,8 @@ public class ClaseControladora {
     private List<Producto> listaProductos = new ArrayList<>();
     private List<Usuario> listaUsuarios = new ArrayList<>();
     private List<VentaProductos> listaVentaProductos = new ArrayList<>();
+    private List<CarroCompra> listaCarritos = new ArrayList<>();
+
 
     private int contadorProducto = 1;
     private int contadorUsuario = 1;
@@ -21,7 +24,6 @@ public class ClaseControladora {
 
     private ClaseControladora() {
 
-        listaProductos.add( new Producto(1, "PC", new BigDecimal("40000")));
         listaUsuarios.add(new Usuario("admin", "elianny", "admin"));
 
     }
@@ -56,5 +58,33 @@ public class ClaseControladora {
     private int generarIdVenta() {
         return contadorVenta++;
     }
+
+    public Producto crearProducto(Producto p) {
+        p.setId(generarIdProducto());
+        listaProductos.add(p);
+
+        return p;
+    }
+
+    public Producto eliminarProducto(int id) {
+        Producto p = buscarProducto(id);
+
+        if (p != null) {
+            listaProductos.remove(p);
+        }
+
+        return p;
+    }
+
+    public Producto buscarProducto(int id) {
+        for (Producto p : listaProductos) {
+            if(p.getId() == id) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+
 
 }
