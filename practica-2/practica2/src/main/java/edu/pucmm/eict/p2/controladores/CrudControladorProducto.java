0b06589cee03f613cf1d3f88dbc9909189bbef87
Elianny Urena <eliannyurenafdez17@gmail.com>
2026-06-14 23:17:53 -0400

    Agregando metodo listar

--- practica-2/practica2/src/main/java/edu/pucmm/eict/p2/controladores/CrudControladorProducto.java
+++ practica-2/practica2/src/main/java/edu/pucmm/eict/p2/controladores/CrudControladorProducto.java
@@ -1,4 +1,27 @@
 package edu.pucmm.eict.p2.controladores;
 
+import edu.pucmm.eict.p2.entidades.Producto;
+import edu.pucmm.eict.p2.servicios.ClaseControladora;
+
+import javax.naming.Context;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CrudControladorProducto {
+
+    private final static ClaseControladora claseControladora = ClaseControladora.getInstancia();
+
+    public static void listarProductos(Context ctx) {
+
+        List<Producto> productos = claseControladora.getListaProductos();
+        Map<String, Object> modelo = new HashMap<>();
+
+        modelo.put("titulo", "Listado de productos");
+        modelo.put("listaProductos", productos);
+
+        //renderizar
+
+
+    }
 }
