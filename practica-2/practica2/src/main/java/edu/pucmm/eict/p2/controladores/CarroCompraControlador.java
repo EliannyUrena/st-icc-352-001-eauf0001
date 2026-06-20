39c07328df4e3b6405574fbfe22d160eaa2bb210
Elianny Urena <eliannyurenafdez17@gmail.com>
2026-06-20 14:42:32 -0400

    Agregando la clase carroCompraControlador

---
+++ practica-2/practica2/src/main/java/edu/pucmm/eict/p2/controladores/CarroCompraControlador.java
@@ -0,0 +1,29 @@
+package edu.pucmm.eict.p2.controladores;
+
+import edu.pucmm.eict.p2.entidades.CarroCompra;
+import edu.pucmm.eict.p2.servicios.ClaseControladora;
+import io.javalin.http.Context;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CarroCompraControlador {
+
+    private final static ClaseControladora claseControladora = ClaseControladora.getInstancia();
+
+    public static void verCarrito(Context ctx) {
+
+        String nombreCliente = ctx.sessionAttribute("nombreCliente");
+        CarroCompra carroCompra = claseControladora.obtenerCarrito(nombreCliente);
+
+        Map<String, Object> modelo = new HashMap<>();
+        modelo.put("titulo", "Carrito de compra");
+        modelo.put("carroCompra", carroCompra.getListaProductos());
+        //modelo.put("total", claseControladora.obtenerSubTotal());
+        modelo.put("nombreCliente", nombreCliente);
+
+        ctx.render("/templates/carroCompra.html");
+
+    }
+
+}
