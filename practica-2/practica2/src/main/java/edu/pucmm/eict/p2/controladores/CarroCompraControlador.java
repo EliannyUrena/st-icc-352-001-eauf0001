bd514e5bad3395609c96ba6566f0afb58a0a465c
Elianny Urena <eliannyurenafdez17@gmail.com>
2026-06-20 21:39:44 -0400

    Agregando metodos para agregar productos al carrito y procesar la compra

--- practica-2/practica2/src/main/java/edu/pucmm/eict/p2/controladores/CarroCompraControlador.java
+++ practica-2/practica2/src/main/java/edu/pucmm/eict/p2/controladores/CarroCompraControlador.java
@@ -1,12 +1,16 @@
 package edu.pucmm.eict.p2.controladores;
 
 import edu.pucmm.eict.p2.entidades.CarroCompra;
+import edu.pucmm.eict.p2.entidades.DetalleCarrito;
+import edu.pucmm.eict.p2.entidades.Producto;
 import edu.pucmm.eict.p2.servicios.ClaseControladora;
 import io.javalin.http.Context;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class CarroCompraControlador {
 
@@ -14,16 +18,72 @@ public class CarroCompraControlador {
 
     public static void verCarrito(Context ctx) {
 
-        String nombreCliente = ctx.sessionAttribute("nombreCliente");
-        CarroCompra carroCompra = claseControladora.obtenerCarrito(nombreCliente);
+            CarroCompra carroCompra = ctx.sessionAttribute("carroCompra");
+            System.out.println("Carrito en verCarrito: " + carroCompra);
+            if (carroCompra == null) {
+                carroCompra = new CarroCompra();
+            }
 
-        Map<String, Object> modelo = new HashMap<>();
-        modelo.put("titulo", "Carrito de compra");
-        modelo.put("carroCompra", carroCompra.getListaProductos());
-        modelo.put("total", claseControladora.calcularTotal(carroCompra));
-        modelo.put("nombreCliente", nombreCliente);
+            Map<String, Object> modelo = new HashMap<>();
+            modelo.put("titulo", "Carrito de compra");
+            modelo.put("carroCompra", carroCompra.getListaProductos());
+            modelo.put("total", claseControladora.calcularTotal(carroCompra));
 
-        ctx.render("/templates/carroCompra.html", modelo);
+            System.out.println("Cantidad productos: " + carroCompra.getListaProductos().size());
+
+            ctx.render("/templates/carroCompra.html", modelo);
+
+    }
+
+    public static void procesarCompra(Context ctx) {
+
+        IO.println("Procesar compra");
+
+        String nombreCliente = ctx.formParam("nombreCliente");
+
+        if (nombreCliente == null) {
+            ctx.redirect("/carroCompra/ver");
+            return;
+        }
+
+        CarroCompra carroCompra = ctx.sessionAttribute("carroCompra");
+
+        if (carroCompra == null || carroCompra.getListaProductos().isEmpty()) {
+            ctx.redirect("/carroCompra/ver");
+            return;
+        }
+
+        carroCompra.setNombreCliente(nombreCliente);
+
+        claseControladora.getListaCarritos().add(carroCompra);
+
+        ctx.sessionAttribute("carroCompra", new CarroCompra());
+
+        ctx.redirect("/");
+    }
+
+
+    public static void agregarProductoCarrito(Context ctx) {
+
+        int idProducto = ctx.pathParamAsClass("id", Integer.class).required().get();
+        int cantidad = ctx.formParamAsClass("cantidad", Integer.class).get();
+        Producto producto = claseControladora.buscarProducto(idProducto);
+
+        CarroCompra carroCompra = ctx.sessionAttribute("carroCompra");
+
+        if (carroCompra == null) {
+            carroCompra = new CarroCompra();
+            carroCompra.setListaProductos(new ArrayList<>());
+        }
+
+        claseControladora.agregarProductoCarrito(carroCompra, producto, cantidad);
+
+        System.out.println("id: " + idProducto);
+        System.out.println("cant: " + cantidad);
+        System.out.println("Cantidad en carrito: " + carroCompra.getListaProductos().size());
+
+        ctx.sessionAttribute("carroCompra", carroCompra);
+        ctx.redirect("/carroCompra/ver");
 
     }
 
