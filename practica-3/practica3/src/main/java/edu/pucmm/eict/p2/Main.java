3d4c9e04732b74e22bd40fea2ff0f7b7da248672
Elianny Urena <eliannyurenafdez17@gmail.com>
2026-06-21 19:38:34 -0400

    Agregando proyecto base

---
+++ practica-3/practica3/src/main/java/edu/pucmm/eict/p2/Main.java
@@ -0,0 +1,100 @@
+package edu.pucmm.eict.p2;
+
+import edu.pucmm.eict.p2.controladores.CarroCompraControlador;
+import edu.pucmm.eict.p2.controladores.CrudControladorProducto;
+import edu.pucmm.eict.p2.controladores.LoginControlador;
+import edu.pucmm.eict.p2.entidades.CarroCompra;
+import edu.pucmm.eict.p2.entidades.Usuario;
+import edu.pucmm.eict.p2.servicios.ClaseControladora;
+import io.javalin.Javalin;
+
+
+import io.javalin.rendering.template.JavalinThymeleaf;
+
+import static io.javalin.apibuilder.ApiBuilder.*;
+import static io.javalin.apibuilder.ApiBuilder.delete;
+import static io.javalin.apibuilder.ApiBuilder.get;
+import static io.javalin.apibuilder.ApiBuilder.path;
+import static io.javalin.apibuilder.ApiBuilder.post;
+import static io.javalin.apibuilder.ApiBuilder.put;
+
+
+public class Main {
+
+    void main() {
+
+        ClaseControladora claseControladora = ClaseControladora.getInstancia();
+
+        Javalin app = Javalin.create(config -> {
+
+            config.fileRenderer(new JavalinThymeleaf());
+
+            config.staticFiles.add(staticFileConfig -> {
+                staticFileConfig.hostedPath = "/";
+                staticFileConfig.directory = "/publico";
+            });
+
+            LoginControlador.aplicarRutas(config);
+
+            config.routes.before(ctx -> {
+                CarroCompra carroCompra = ctx.sessionAttribute("carroCompra");
+
+                int cantidad = claseControladora.obtenerCantidadProductosCarrito(carroCompra);
+
+                ctx.attribute("cantidad", cantidad);
+            });
+
+            config.routes.before("/crudProductos/**", ctx -> {
+                Usuario usuario = ctx.sessionAttribute("usuario");
+
+                if (usuario == null || (!usuario.getUsuario().equals("admin") && !usuario.getPassword().equals("admin"))){
+                    ctx.redirect("/login.html");
+                }
+            });
+
+            config.routes.apiBuilder( () -> {
+                get("/listarProductos", CrudControladorProducto::listarProductos);
+
+            });
+
+
+            config.routes.apiBuilder(() -> {
+
+                path("/crudProductos", () -> {
+
+                    get("/crear", CrudControladorProducto::crearProductosForm);
+                    post("/crear", CrudControladorProducto::procesarCrearProductos);
+
+                    get("/editar/{id}", CrudControladorProducto::editarProductosForm);
+                    post("/editar", CrudControladorProducto::procesarEditarProducto);
+
+                    get("/eliminar/{id}", CrudControladorProducto::eliminarProducto);
+
+                    get("/listarVentas", CarroCompraControlador::listarVentas);
+                });
+
+                path("/carroCompra", () -> {
+                    get("/ver", CarroCompraControlador:: verCarrito);
+                    post("/procesar", CarroCompraControlador::procesarCompra);
+                    post("/agregar/{id}", CarroCompraControlador::agregarProductoCarrito);
+                    get("/eliminar/{id}", CarroCompraControlador::eliminarProductoCarrito);
+                    get("/limpiar", CarroCompraControlador::limpiarCarrito);
+                });
+
+            });
+
+            /*
+            config.routes.get("/", ctx -> {
+                ctx.result("Hola");
+
+            });*/
+
+            config.routes.get("/", CrudControladorProducto::listarProductos);
+
+        });
+
+        app.start(7000);
+
+    }
+}
+
---
+++ practica-3/practica3/src/main/java/edu/pucmm/eict/p2/entidades/VentaProductos.java
@@ -0,0 +1,74 @@
+package edu.pucmm.eict.p2.entidades;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class VentaProductos {
+
+    private long id;
+    private Date fechaCompra;
+    private String nombreCliente;
+    private List<DetalleCarrito> listaProductos;
+
+    public VentaProductos() {
+
+        this.listaProductos = new ArrayList<>();
+        this.fechaCompra = new Date();
+    }
+
+    public VentaProductos(long id, String nombreCliente) {
+        this.id = id;
+        this.fechaCompra = new Date();
+        this.nombreCliente = nombreCliente;
+        this.listaProductos = new ArrayList<>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public void setFechaCompra(Date fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public List<DetalleCarrito> getListaProductos() {
+        return listaProductos;
+    }
+
+    public void setListaProductos(List<DetalleCarrito> listaProductos) {
+        this.listaProductos = listaProductos;
+    }
+
+    public BigDecimal calcularTotalProductosVenta() {
+        BigDecimal total = BigDecimal.valueOf(0);
+
+        for (DetalleCarrito detalle : listaProductos) {
+            BigDecimal subtotal = detalle.getProducto().getPrecio().multiply(BigDecimal.valueOf(detalle.getCantidad()));
+
+            total = total.add(subtotal);
+
+        }
+        return total;
+    }
+
+
+
+}
