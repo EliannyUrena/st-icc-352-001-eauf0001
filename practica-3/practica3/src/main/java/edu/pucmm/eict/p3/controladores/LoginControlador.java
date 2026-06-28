d3a251a1618a74280f32e44405255ad559a4ff53
Elianny Urena <eliannyurenafdez17@gmail.com>
2026-06-28 15:38:32 -0400

    Creando cookie

--- practica-3/practica3/src/main/java/edu/pucmm/eict/p3/controladores/LoginControlador.java
+++ practica-3/practica3/src/main/java/edu/pucmm/eict/p3/controladores/LoginControlador.java
@@ -1,8 +1,13 @@
 package edu.pucmm.eict.p3.controladores;
 
+import edu.pucmm.eict.p3.servicios.ProductoServices;
+import edu.pucmm.eict.p3.servicios.UsuarioServices;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import edu.pucmm.eict.p3.controladores.Encriptador;
 import edu.pucmm.eict.p3.entidades.Usuario;
 import edu.pucmm.eict.p3.servicios.ClaseControladora;
 import io.javalin.config.JavalinConfig;
+import io.javalin.http.Context;
 
 public class LoginControlador {
 
@@ -12,11 +17,16 @@ public class LoginControlador {
 
             String usuario = ctx.formParam("usuario");
             String password = ctx.formParam("password");
+            String recordar = ctx.formParam("recordarUsuario");
 
-            Usuario user = ClaseControladora.getInstancia().autenticarUsuario(usuario, password);
+            Usuario user = UsuarioServices.getInstancia().autenticarUsuario(usuario, password);
 
             if (user != null ) {
                 ctx.sessionAttribute("usuario", user);
+
+                if (recordar != null) {
+                    crearCookie(ctx, user);
+                }
 
                 ctx.redirect("/");
             } else {
@@ -29,10 +39,21 @@ public class LoginControlador {
         });
 
         config.routes.get("/logout", ctx -> {
-           ctx.sessionAttribute("usuario", null);
-           ctx.redirect("/");
+           //ctx.sessionAttribute("usuario", null);
+            ctx.req().getSession().invalidate();
+
+            ctx.removeCookie("usuario");
+            ctx.redirect("/");
         });
 
 
+    }
+
+    public static void crearCookie(Context ctx, Usuario usuario) {
+        String user = usuario.getUsuario();
+        Encriptador encriptador = new Encriptador();
+
+        String encriptarUsuario = encriptador.encriptar(user);
+        ctx.cookie("usuario", encriptarUsuario, 604800);
     }
 }
